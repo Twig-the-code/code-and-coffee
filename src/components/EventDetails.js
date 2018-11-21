17373c1f11965a29ce4bf69ef4d89e277f350cae
Heidi Vainio-Pekka <heidi.vainiopekka@gmail.com>
2018-11-21 19:55:33 +0200

    place holder done

---
+++ src/components/EventDetails.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './EventDetails.css';
+
+class EventDetails extends Component {
+  render() {
+    return (
+      <div className="event-details">
+        <p>placeholder</p>
+
+        
+      </div>
+    );
+  }
+}
+
+export default EventDetails;
