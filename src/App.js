6b9068a99463bdda23284eac9d21aba7ebf672e7
Emmi K <koivunen.emmi@gmail.com>
2018-11-21 20:48:20 +0200

    State-konsepti apille

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import EventDetails from './components/EventDetails';
 import NameList from './components/NameList';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      names : ["a","b","c","d","skorg"]
+    }
+  }
+  handleClick(event){
+    const names = this.state.names.concat("apua")
+    this.setState({
+      names
+    })
+    console.log("apua")
+  }
   render() {
-    const names = ["a","b","c","d","skorg"];
     return (
       <div className="App">
         <EventDetails></EventDetails>
+        <input type = "button" onClick = {e => this.handleClick(e)}></input>
         <Register></Register>
         <NameList 
-          names = {names}
+          names = {this.state.names}
         />
       </div>
     );
