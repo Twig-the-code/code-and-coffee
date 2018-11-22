9cabf906eb55ceb5005430362144fa860c5471b2
eevisk <eeva.kivila@gmail.com>
2018-11-22 14:25:14 +0200

    Update EventDetails.js

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -6,7 +6,8 @@ class EventDetails extends Component {
   render() {
     return (
       <div className="event-details">
-        <p>placeholder</p>
+        <p><h1>Code and Coffee</h1></p>
+        <p>27.11.2018 klo 8.30-9.15</p>
 
         
       </div>
