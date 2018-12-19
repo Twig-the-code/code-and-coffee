fb5668569168744b804dbe0e979cf48b2f28c70f
Aki Salmi <aki.salmi@iki.fi>
2018-12-19 19:35:34 +0200

    fix conflicts

--- src/components/ChristmasCard.js
+++ src/components/ChristmasCard.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import PropTypes from 'prop-types'
-=======
 import PropTypes from 'prop-types';
->>>>>>> defaultProps  ChristmasCard
 import './ChristmasCard.css';
 
 class ChristmasCard extends Component {
@@ -19,11 +15,7 @@ class ChristmasCard extends Component {
                 <div class="tag-side tag-1-side">
                     <div class="tag-1-top"></div>
                     <div class="tag-text tag-1-text">
-<<<<<<< HEAD
-                    {this.props.cardOne}
-=======
                         {this.props.cardOne}
->>>>>>> defaultProps  ChristmasCard
                         <div class="rule-shape rule-red">&#9733;</div>
                     </div>
                 </div>
@@ -44,22 +36,14 @@ class ChristmasCard extends Component {
     
                 <div class="tag-side tag-2-side">
                     <div class="tag-text tag-2-text">
-<<<<<<< HEAD
-                    {this.props.cardTwo}
-=======
                         {this.props.cardTwo}
->>>>>>> defaultProps  ChristmasCard
                         <div class="rule-diagonal"></div>
                     </div>
                 </div>
     
                 <div class="tag-side tag-2-side is-back">
                     <div class="tag-text tag-2-text">
-<<<<<<< HEAD
-                    {this.props.cardTwoBack}
-=======
                         {this.props.cardTwoBack}
->>>>>>> defaultProps  ChristmasCard
                         <div class="rule-diagonal"></div>
                     </div>
                 </div>
@@ -72,11 +56,7 @@ class ChristmasCard extends Component {
     
                 <div class="tag-side tag-3-side">
                     <div class="tag-text tag-3-text">
-<<<<<<< HEAD
-                    {this.props.cardThree}
-=======
                         {this.props.cardThree}
->>>>>>> defaultProps  ChristmasCard
                         <div class="rule-shape">&#10052;</div>
                     </div>
                 </div>
@@ -97,16 +77,9 @@ class ChristmasCard extends Component {
 }
 ChristmasCard.defaultProps = {
     cardOne: 'Hyvää joulua',
-<<<<<<< HEAD
-    cardOneBack: 'Ja hyvää uutta vuotta!',
-    cardTwo: 'Iloista joulunaikaa',
-    cardTwoBack: 'Toteutukoon kaikki toiveesi',
-    cardThree: 'God Jul'
-=======
     cardOneBack: 'ja mainiota uutta vuotta!',
     cardTwo: 'Suklaata ja glögiä',
     cardTwoBack: 'lahjoja ja parasta seuraa!',
     cardThree: 'Vietä ihanin joulu!', 
->>>>>>> defaultProps  ChristmasCard
 }
 export default ChristmasCard;
