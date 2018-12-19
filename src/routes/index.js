8d761596ffa429a21d5f001905e7308f844d4882
Anna <annamariakoivisto@gmail.com>
2018-12-19 18:06:33 +0200

    ChristmasCard

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
 import App from "../App";
-
+import ChristmasCard from "../components/ChristmasCard"
 
 
 
 
 export default <Switch>
   <Route exact path="/" component={App} />
+  <Route exact path="/anna" component={ChristmasCard} />
 </Switch>
