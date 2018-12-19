43edafcbd19c8045b052ec942713c90f9055ac1f
Aki Salmi <aki.salmi@iki.fi>
2018-12-19 22:49:15 +0200

    remove old code

--- src/base.js
+++ src/base.js
@@ -1,7 +1,4 @@
-import Rebase from "re-base";
-// import firebase from "firebase";
 import firebase from "firebase/app";
-// import 'firebase/database';
 import "firebase/firestore";
 
 export const FirebaseConfig = {
@@ -16,15 +13,3 @@ export const db = app.firestore()
 db.settings({
   timestampsInSnapshots: true
 });
-// const db = app.database();
-// export const db = firebase.database(app);
-// console.log({db})
-// const base = Rebase.createClass(db);
-
-// This is a named export
-// export { firebaseApp };
-
-// this is a default export
-// export default base;
-
-
