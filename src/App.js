4bca43f6f12d5b63814f8605358110e7f409ed77
Aki Salmi <aki.salmi@iki.fi>
2018-12-19 17:26:49 +0200

    adding React Router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-import Register from "./components/Register"
 import logo from './logo.svg';
 import './App.css';
-import EventDetails from './components/EventDetails';
-import NameList from './components/NameList';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      names : ["a","b","c","d","skorg"]
-    }
-  }
-  handleClick(event){
-    const names = this.state.names.concat("apua")
-    this.setState({
-      names
-    })
-    console.log("apua")
-  }
   render() {
+
+
     return (
       <div className="App">
-        <EventDetails></EventDetails>
-        <input type = "button" onClick = {e => this.handleClick(e)}></input>
-        <Register></Register>
-        <NameList 
-          names = {this.state.names}
-        />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <div className="click-me">
+              Edit <code>src/App.js</code> and save to reload.
+            </div>
+          </p>
+          <a
+            className="App-link "
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="punainen" id="this-is-important">paragraph</p>
+          </a>
+          <input className="click-me" type="button" value='clickMe' />
+        </header>
       </div>
-    );
+  );
   }
-}
+  }
 
-export default App;
+  export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createBrowserHistory } from 'history'
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router'
+import { Provider } from "react-redux"
+import createSagaMiddleware from "redux-saga"
+import {createStore, applyMiddleware, compose} from "redux"
+
+import createRootReducer from "./reducers"
+import {rootSaga} from "./saga"
+import routes  from "./routes"
+
+const history = createBrowserHistory()
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  createRootReducer(history),
+  compose(
+    applyMiddleware(
+      routerMiddleware(history),
+      sagaMiddleware
+    )
+  )
+)
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(<Provider store={store}>
+  <ConnectedRouter history={history}>
+    {routes}
+  </ConnectedRouter>
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
