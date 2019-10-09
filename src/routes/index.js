608125bcfe2d0802bb5fdea4443f08f39251c36d
Riikka-V <Riikka-V@users.noreply.github.com>
2019-10-09 18:18:36 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
 import App from "../App";
-import ChristmasCard from "../components/ChristmasCard"
+
 
 
 
 
 export default <Switch>
   <Route exact path="/" component={App} />
-  <Route exact path="/:name" component={ChristmasCard} />
 </Switch>
